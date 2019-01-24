2029db0369bc764160bf9eaa4ce317fef7e51f9b
Depth1024 <794583971@qq.com>
2019-01-24 21:34:36 +0800

    Swift测试demo
    初次使用swift编写iOS_APP，使用此demo进行一些测试与适应

---
+++ TestDemo_Swift/TestDemo_Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestDemo_Swift
+//
+//  Created by zlr on 2019/1/24.
+//  Copyright © 2019 zlr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
