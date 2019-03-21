89959771e003e06c095126e39e734c71192fc399
Depth1024 <794583971@qq.com>
2019-03-21 10:48:43 +0800

    change

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//   左侧骰子
+    @IBOutlet weak var diceImageView1: UIImageView!
+// 建立IBOutlet关联的时候，如果正常建立，则代码中IBOutlet变量前会有一个实心圆圈（鼠标悬停时，与之关联的控件会高亮）；如果还没有与控件进行关联，那么该圆圈为空心圆。
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
